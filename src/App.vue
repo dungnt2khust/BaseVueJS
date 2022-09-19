82290ddafa2f24e3847f78122c085647904b622d
dungnt2khust <63250067+dungnt2khust@users.noreply.github.com>
2022-09-19 08:29:15 +0700

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App">
+      <template v-slot:header="{ row }">
+        <h1>{{ row }}</h1>
+        <h2>DQVINH</h2>
+      </template>
+    </HelloWorld>
   </div>
 </template>
 
